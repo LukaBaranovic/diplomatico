d5bd7b699375ce47019a869a061128ea5eceb3f9
Luka Baranovic <lukabaranovic366@gmail.com>
2024-12-09 17:05:01 +0100

    delete

--- delete-category.php
+++ delete-category.php
@@ -5,8 +5,6 @@ session_start();
 $mysqli = require_once __DIR__ . "/database.php";
 
 $category_id = (int)$_POST['category_id'];
-$category_id = mysqli_real_escape_string($mysqli, $category_id);
-
 
 // Provjeravamo ima li kategorija koju brišemo children elemenata 
 $query_check_for_children = "SELECT COUNT(*) FROM item WHERE category_id = ?";
@@ -17,8 +15,13 @@ $stmt_select_check_for_children->bind_result($item_count);
 $stmt_select_check_for_children->fetch();
 $stmt_select_check_for_children->close();
 
+echo $item_count;
+
 // Ako ima children elemenata, vracamo se na početnu stranicu.
 if ($item_count == 0) {
+
+  $query = "DELETE FROM `category` WHERE `category_id` = ?";
+
   // Provjerava imamo li category_id
   if (isset($_POST['category_id'])) {
     
